9a11b379b529ec12bd76bc07ea0858e8b3d6e753
Clockmere <henrique.p2009@aluno.ifsc.edu.br>
2026-07-22 17:09:39 -0300

    Java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+   public static void main (String[] args) {
+      System.out.println("Hello, World!");
+
+      int num1 = 2;
+      int num2 = 5;
+
+      int resultado = num1 + num2;
+
+      System.out.print(num1 + " + " + num2 +
+      " = " + resultado + "\n");
+   } 
+}
